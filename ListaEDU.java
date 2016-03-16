89eff480bc2d7492e0061a488905fbd9554b1a43
theeam <eduuardomuller@gmail.com>
2016-03-16 19:36:54 -0300

    //TODO delete

--- ListaEDU.java
+++ ListaEDU.java
@@ -50,18 +50,58 @@ public class Lista {
 		}
 	}
 
+	public static Node searchElement(int info) {
+		if (inicio != null) {
+			auxiliar = inicio; // Aponta o auxiliar para o inicio da lista
+			for (int i = 0; i < contador; i++) {
+				if (auxiliar.getInfo() == info) { // Verifica se a info do node
+													// e igual a recebida
+					return auxiliar; // Caso seja, imprimi o node
+				}
+				auxiliar = auxiliar.getNext(); // Aponta o auxiliar para o
+												// proximo node
+			}
+		}
+		return null;
+
+	}
+
+	public static Node searchXElement(int n) {
+		if (inicio != null) { // Verifica se a lista e vazia
+			auxiliar = inicio; // Aponta o auxiliar para o inicio
+			for (int i = 0; i < n - 1; i++) {
+				auxiliar = auxiliar.getNext(); // Aponta o auxiliar para o
+												// proximo ate chegar a posicao
+												// desejada
+			}
+
+			return auxiliar;// Imprimi o node buscado
+		}
+		return null;
+	}
+
+	public static void deleteElement(int info) {//TODO
+		auxiliar = searchElement(info);
+		if(auxiliar != null){
+			
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Insira um numero: ");
 		int x = 0;
+
 		do {
 			addElement(sc.nextInt());
 			System.out.println("outro: ");
 			x++;
-		} while (x < 10);
+		} while (x <= 10);
 
 		System.out.println("Wait...");
 		printList();
+		System.out.println(searchElement(3));
+		System.out.println(searchXElement(3));
 		cleanList();
 		printList();
 	}
