b7ab7cf4e9b8ca07d3861dddc7e66e626813a14c
beccinha <rebeccanascimento1@hotmail.com>
2016-03-23 17:38:34 -0300

    Update BeccaLista.java

--- BeccaLista.java
+++ BeccaLista.java
@@ -1,14 +1,3 @@
-import org.omg.CORBA.Context;
-import org.omg.CORBA.ContextList;
-import org.omg.CORBA.DomainManager;
-import org.omg.CORBA.ExceptionList;
-import org.omg.CORBA.NVList;
-import org.omg.CORBA.NamedValue;
-import org.omg.CORBA.Object;
-import org.omg.CORBA.Policy;
-import org.omg.CORBA.Request;
-import org.omg.CORBA.SetOverrideType;
-import org.omg.SendingContext.RunTime;
 
 public class Lista {
 
